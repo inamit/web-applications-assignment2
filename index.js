010d55e5a326b409a6c29c3b7be38abf5749b47a
Amit Inbar <inamit323@gmail.com>
2024-11-15 17:49:15 +0200

    feat: add body-parser

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require("express");
-
 const { connectDB } = require("./db");
 connectDB();
 
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
