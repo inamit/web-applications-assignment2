b9eacc692569a90307dfbdd763c496665ae1e546
Amit Inbar <inamit323@gmail.com>
2024-11-15 17:54:22 +0200

    feat: get all posts

---
+++ controllers/post.js
@@ -0,0 +1,12 @@
+const Post = require("../models/post");
+
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { getAllPosts };
