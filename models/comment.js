e9e87216f2bd97f7e2e2604e55bc02dc8a2827f5
Amit Inbar <inamit323@gmail.com>
2024-11-21 19:02:33 +0200

    feat: make postId reference

--- models/comment.js
+++ models/comment.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-    postID: {
-        type: String,
-        required: true,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
-    sender: {
-        type: String,
-        required: true,
-    }
+  postID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
