e4a6b120f9cf4de7f55488355d2ebc097d199df3
Amit Inbar <inamit323@gmail.com>
2024-11-30 18:40:17 +0200

    fix: fix save password hash

--- models/users_model.js
+++ models/users_model.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function (next) {
-  hashPassword(this.password);
+  this.password = await hashPassword(this.password);
   next();
 });
 
