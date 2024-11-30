6be96ce08554d31cf1c29c18c371e7f3e4f90043
Benli <e@example.com>
2024-12-01 01:29:22 +0200

    create user unit tests

---
+++ tests/users.test.js
@@ -0,0 +1,234 @@
+const request = require("supertest");
+const initApp = require("../server.js");
+const mongoose = require("mongoose");
+const usersModel = require("../models/users_model.js");
+
+
+let app;
+beforeAll(async () => {
+  app = await initApp();
+});
+beforeEach(async () => {
+  await usersModel.deleteMany();
+});
+
+afterAll(async () => {
+  mongoose.connection.close();
+});
+
+const testUsers = [
+    { username: "Benli", email: "first@gmail.com", password: "password" },
+    { username: "Amit", email: "second@gmail.com", password: "password" },
+];
+
+describe("GET /users", () => {
+    describe("when there are no users", () => {
+        it("should return an empty array", async () => {
+        const response = await request(app).get("/users");
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body).toHaveLength(0);
+        });
+    });
+
+    describe("when there are users", () => {
+        beforeEach(async () => {
+            await usersModel.create(testUsers);
+        });
+
+        it("should return all users", async () => {
+            const response = await request(app).get("/users");
+
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toBeInstanceOf(Array);
+            expect(response.body).toHaveLength(testUsers.length);
+        });
+    });
+
+    describe("mongo failure", () => {
+        it("should return 500 when there is a server error", async () => {
+            jest
+                .spyOn(usersModel, "find")
+                .mockRejectedValue(new Error("Server error"));
+
+            const response = await request(app).get("/users");
+
+            expect(response.statusCode).toBe(500);
+            expect(response.body).toHaveProperty("error");
+        });
+    });
+});
+
+describe("POST /users", () => {
+    it("should register new user", async () => {
+        const username = "Benli";
+        const email = "amitinbar@gmail.com";
+        const password = "myPassword"
+        const response = await request(app).post("/users").send({
+            username,
+            email,
+            password
+        });
+    
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty("_id");
+        expect(response.body.username).toBe(username);
+        expect(response.body.email).toBe(email);
+    });
+
+    it.each([{ username: "No username" }, { email: "No email" }, { password: "No password" }, {}])(
+        "should return 400 when parameter is missing (%o)",
+        async ({ username, email, password}) => {
+            const response = await request(app).post("/posts").send({
+                username,
+                email,
+                password
+            });
+
+            expect(response.statusCode).toBe(400);
+            expect(response.body).toHaveProperty("error");
+        }
+    );
+});
+
+
+describe("GET /users/:user_id", () => {
+    let savedUsers = [];
+    beforeEach(async () => {
+        savedUsers = await usersModel.create(testUsers);
+    });
+
+    it("should return 404 when user is not found", async () => {
+        const response = await request(app).get("/users/6749f45d349ed9de3a163158");
+
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toHaveProperty("error");
+    });
+
+    it("should return 400 when user_id is invalid", async () => {
+        const response = await request(app).get("/users/invalid_id");
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("error");
+    });
+
+    it("should return user by id", async () => {
+        const user = savedUsers[0];
+        const response = await request(app).get(`/users/${user._id}`);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toMatchObject(JSON.parse(JSON.stringify(user)));
+    });
+});
+
+describe("PUT /users/:user_id", () => {
+    let savedUsers = [];
+    beforeEach(async () => {
+        savedUsers = await usersModel.create(testUsers);
+    });
+
+    it("should return 404 when user is not found", async () => {
+        const response = await request(app)
+        .put("/users/6749f45d349ed9de3a163155")
+        .send({
+            username: "Updated user",
+            email: "updated@gmail.com",
+            password: "updated"
+        });
+
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toHaveProperty("error");
+    });
+
+    it("should return 400 when user_id is invalid", async () => {
+        const response = await request(app).put("/users/invalid_id")
+        .send({
+            username: "Updated user",
+            email: "updated@gmail.com",
+            password: "updated"
+        });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("error");
+    });
+
+    it.each([{ username: "No username" }, { email: "No email" }, { password: "No password" }, {}])(
+        "should return 400 when parameter is missing (%o)",
+        async ({ username, email, password }) => {
+        const post = savedUsers[0];
+        const response = await request(app)
+            .put(`/users/${user._id}`)
+            .send({ username, email, password });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("error");
+        }
+    );
+
+    it("should update user by id", async () => {
+        const post = savedUsers[0];
+        const updatedUsername = "Updated username";
+        const updatedEmail = "updated@gmail.com";
+        const updatedPassword = "password"
+        const response = await request(app)
+        .put(`/posts/${post._id}`)
+        .send({ username: updatedUsername, email: updatedEmail, password: updatedPassword });
+
+        const isMatchedpassword = await bcrypt.compare(password, response?.password);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.username).toBe(updatedContent);
+        expect(response.body.email).toBe(updatedSender);
+        expect(isMatchedpassword).toBe(true);
+    });
+});
+
+describe('POST /users/login', () => {
+    let savedUsers = [];
+    beforeEach(async () => {
+        savedUsers = await usersModel.create(testUsers);
+    });
+    it('should return a response with a non-empty cookie after user is logged in', async () => {
+        const username = "Benli";
+        const email = "first@gmail.com";
+        const password = "password"
+        const response = await request(app).post("/users/login").send({
+            username,
+            email,
+            password
+        });
+        const cookies = response.headers['set-cookie'];
+        expect(cookies).toBeDefined();
+        expect(cookies.length).toBeGreaterThan(0);
+        const accessTokenCookie = cookies.find(cookie => cookie.startsWith('accessToken='));
+        expect(accessTokenCookie).toBeDefined();
+        const cookieValue = accessTokenCookie.split('=')[1].split(';')[0];
+        expect(cookieValue).toBeTruthy();
+    });
+    it("should return 400 when wrong credentials", async () => {
+        const username = "Benli";
+        const email = "first@gmail.com";
+        const password = "ppaassword"
+        const response = await request(app).post("/users/login").send({
+            username,
+            email,
+            password
+        });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("error");
+    });
+});
+
+describe('POST /users/logout', () => {
+    it('should return a response with a empty cookie after user is logged out', async () => {
+        const response = await request(app).post("/users/logout").send({});
+        const cookies = response.headers['set-cookie'];
+        expect(cookies).toBeDefined();
+        expect(cookies.length).toBeGreaterThan(0);
+        const accessTokenCookie = cookies.find(cookie => cookie.startsWith('accessToken='));
+        expect(accessTokenCookie).toBeDefined();
+        const cookieValue = accessTokenCookie.split('=')[1].split(';')[0];
+        expect(cookieValue).toBe('')
+    });
+});
